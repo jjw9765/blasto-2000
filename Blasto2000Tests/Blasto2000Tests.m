8f3997bbc9df532123893ce30cfb7235df11f207
JJ Watson <jjw9765@rit.edu>
2015-04-07 21:34:29 -0400

    Removed background music

---
+++ Blasto2000Tests/Blasto2000Tests.m
@@ -0,0 +1,34 @@
+//
+//  Blasto2000Tests.m
+//  Blasto2000Tests
+//
+//  Created by JJ Watson on 10/10/13.
+//  Copyright (c) 2013 John Watson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Blasto2000Tests : XCTestCase
+
+@end
+
+@implementation Blasto2000Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
